dfc1b17319acdb0db5a73acc58f347af5d57a63b
AndreFelipe1D <114994513+AndreFelipe1D@users.noreply.github.com>
2024-06-23 20:31:58 +0000

    alteracoes

---
+++ script.js
@@ -0,0 +1,54 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Entre um Gol CL 1.8 MI 1997 ou Chevette DL 1.6 1992, Qual você escolheria?",
+        alternativas: [
+            "Gol CL 1.8 MI 1997",
+            "Chevette DL 1.6 1992"
+        ]
+    },
+    {
+        enunciado: "Pergunta2",
+        alternativas: [
+            "Alternativa1",
+            "Alternativa2"
+        ]
+    },
+    {
+        enunciado: "Pergunta2",
+        alternativas: [
+            "Alternativa1",
+            "Alternativa2"
+        ]
+    },
+    {
+        enunciado: "Pergunta2",
+        alternativas: [
+            "Alternativa1",
+            "Alternativa2"
+        ]
+    },
+    {
+        enunciado: "Pergunta2",
+        alternativas: [
+            "Alternativa1",
+            "Alternativa2"
+        ]
+    },
+];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+
+mostraPergunta();
+ 
