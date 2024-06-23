5789693114a0485f0c2891cfd7e8d14c19999d56
AndreFelipe1D <114994513+AndreFelipe1D@users.noreply.github.com>
2024-06-23 22:13:48 +0000

    alteracoes

--- script.js
+++ script.js
@@ -8,36 +8,67 @@ const perguntas = [
     {
         enunciado: "Entre um Civic SI 2.0 2008 ou Opala Comodoro 4.1 1992, Qual você escolheria?",
         alternativas: [
-            "Civic SI 2008",
-            "Opala 4.1 1992"
+            {
+                texto: "Civic SI 2008",
+                afirmacao: "afirmacao"
+            },
+            {
+             texto: "Opala 4.1 1992",   
+             afirmacao: "afirmacao"
+            }
         ]
     },
     {
         enunciado: "Carburado ou Injetado?",
         alternativas: [
-            "Carburado",
-            "Injetado"
+            {
+                texto: "Carburado",
+                afirmacao: "afirmacao"
+            },
+            {
+                texto: "Injetado",
+                afirmacao: "afirmacao"
+            }
+            
         ]
     },
     {
         enunciado: "Turbo ou Aspirado?",
         alternativas: [
-            "Turbo",
-            "Aspirado"
+            {
+                texto: "Turbo",
+                afirmacao:"afirmacao"
+            },
+            {
+                texto: "Aspirado",
+                afirmacao:"afirmacao"
+            }
         ]
     },
     {
         enunciado: "Correr à noite em Le Mans ou em Nurburgring?",
         alternativas: [
-            "Le Mans",
-            "Nurburgring"
+            {
+                texto:"Le Mans",
+                afirmacao:"afirmacao"
+            },
+            {
+                texto:"Nurburgring",
+                afirmacao:"afirmacao"
+            }
         ]
     },
     {
         enunciado: "Qual o Melhor Motor para o Skyline R34 do O'Conner?",
         alternativas: [
-            "Gallo 12",
-            "Gallo 24"
+            {
+                texto: "Gallo 12",
+                afirmacao:"afirmacao"
+            },
+            {
+                texto:"Gallo 24",
+                afirmacao:"afirmacao"
+            }
         ]
     },
 ];
@@ -54,7 +85,11 @@ function mostraPergunta() {
 function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", function(){
+            atual++;
+            mostraPergunta();
+        })
         caixaAlternativas.appendChild(botaoAlternativas); 
     }
 }
